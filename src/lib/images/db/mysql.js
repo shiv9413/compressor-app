7a29871d0a6d5667cb1121ecbcdbf3adb95c0810
Gagan Deep Kashyap <cu.gagankashyap@gmail.com>
2024-02-09 18:23:48 +0530

    db connected

---
+++ src/lib/images/db/mysql.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise";
+
+let mysqlconn = null;
+
+export function mysqlconnFn() {
+  if (!mysqlconn) {
+    mysqlconn = mysql.createConnection({
+      host: "162.241.218.208",
+      user: "algyvwmy_state_reader",
+      password: "SveltekitMySQL",
+      database: "algyvwmy_states",
+    });
+  }
+
+  return mysqlconn;
+}
